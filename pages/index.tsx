b5b59c1858b8e7b31fe4bde43126f16ee27196a8
Nithinreddy1538 <nithinkumarreddy1538@gmail.com>
2025-07-25 12:33:12 +0530

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>Hello 👋</h1>
+      <p>This is Nithin's Next.js app deployed on Vercel!</p>
+    </div>
+  );
+}
